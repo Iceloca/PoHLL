d5c78f08f5b20fb9e8dcc744c02795ea53e85980
Aliaksei Knyrovich <6525088@gmail.com>
2024-02-26 14:44:07 +0300

    Test commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Server Checker Web Service
+The Server Checker Service is a Java-based web app for checking server statuses
+
+## Lab1
+1 - Создать и запустить локально простейший веб/REST сервис <br>
+2 - Добавить GET ендпоинт
+## Technologies Used:
+1. Java
+2. Spring Boot
+3. PostgreSQL (or any other preferred database)
+4. Maven
---
+++ src/main/java/iceloca/serverchecker/ServerCheckerApplication.java
@@ -0,0 +1,13 @@
+package iceloca.serverchecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerCheckerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerCheckerApplication.class, args);
+    }
+
+}
